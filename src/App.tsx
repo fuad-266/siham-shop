ba7a98660343aa004997790cd891c564a0958a77
fuad-266 <fuadabdala266@gmail.com>
2026-03-01 23:52:20 -0800

    npm file installed

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <h1>Alora Abayas</h1>
+        <p>E-commerce website coming soon...</p>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
